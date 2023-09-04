32bca7b5fdbe170232cb4e8c9cea33e7d585bc68
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2023-09-04 16:41:49 +0530

    Update Sum of nodes on the longest path from root to leaf node.cpp

--- TREES/Sum of nodes on the longest path from root to leaf node/Sum of nodes on the longest path from root to leaf node.cpp
+++ TREES/Sum of nodes on the longest path from root to leaf node/Sum of nodes on the longest path from root to leaf node.cpp
@@ -1,1 +1,28 @@
-
+void solve(Node *root,int sum,int &maxSum,int len,int&maxLen){
+        //base case reched as we reached leaf node
+         if( root == NULL ) {
+            return;
+        }
+        sum=sum+root->data;
+        
+        if(len>= maxLen){
+            maxLen = len;
+            maxSum = sum;
+        }
+        
+        solve(root->left,sum,maxSum,len+1,maxLen);
+        solve(root->right,sum,maxSum,len+1,maxLen);
+        
+    }
+    
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        int sum=0;
+        int maxSum=INT_MIN;
+        int len=0;
+        int maxLen=0;
+        
+        solve(root,sum,maxSum,len,maxLen);
+        return maxSum;
+    }
+};
