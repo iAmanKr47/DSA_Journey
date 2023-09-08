54595d097e16e65434158f9281ae62009bd6f6af
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2023-09-09 00:21:18 +0530

    Update code.cpp

--- BINARY SERACH TREE (BST)/Kth largest element in BST/code.cpp
+++ BINARY SERACH TREE (BST)/Kth largest element in BST/code.cpp
@@ -1,1 +1,25 @@
-
+ void solve(Node *root, int &K,int&ans){
+        if(root==NULL)
+        return;
+        //go in extreme right first
+        solve(root->right,K,ans);
+        
+        //while returning decrement K BY 1
+        K--;
+        //check for ans if k==0
+        if(K==0)
+        {
+            ans=root->data;
+            return;
+        }
+        // if right se ans nhi mila then go to left
+        solve(root->left,K,ans);
+    }
+        
+    
+    int kthLargest(Node *root, int K)
+    {
+        int ans;
+        solve(root,K,ans);
+        return ans;
+    }
