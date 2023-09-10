2bb0530eb57bd21655cb7ef2978a183eceb2b75b
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2023-09-10 13:08:39 +0530

    Update Code.cpp

--- BINARY SERACH TREE (BST)/INSERT A NODE IN BST/Code.cpp
+++ BINARY SERACH TREE (BST)/INSERT A NODE IN BST/Code.cpp
@@ -1,1 +1,16 @@
-
+Node* insert(Node* node, int data) {
+        
+            if(node == NULL)
+            {
+                Node* newnode = new Node(data); 
+                return newnode;
+            }
+            
+            if(node->data > data)
+                node->left = insert(node->left, data);
+                
+            else if(node->data < data)
+                node->right = insert(node->right, data);
+            
+            return node;
+    }
