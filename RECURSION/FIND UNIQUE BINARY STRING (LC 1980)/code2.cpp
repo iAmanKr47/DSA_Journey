6d719fb4a2a149b477e701c0a7f28ffdec7a2a91
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2023-11-17 21:58:08 +0530

    Create code2.cpp

---
+++ RECURSION/FIND UNIQUE BINARY STRING (LC 1980)/code2.cpp
@@ -0,0 +1,12 @@
+string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+
+        int index = 0;
+        for(auto binary : nums)
+        {
+            ans = ans + to_string('1' - binary[index]);
+            index+=1;
+        }
+        return ans;
+
+    }
