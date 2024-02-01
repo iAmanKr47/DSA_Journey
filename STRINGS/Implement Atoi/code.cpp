8776bb6a8f42d10893ccd455e68f1895e71e7eb7
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-02-02 01:21:46 +0530

    Update code.cpp

--- STRINGS/Implement Atoi/code.cpp
+++ STRINGS/Implement Atoi/code.cpp
@@ -1,3 +1,5 @@
+APPROACH 1:
+      
 int atoi(string s) {
       
        int n=s.size();
@@ -17,4 +19,28 @@ int atoi(string s) {
        }
        if(s[0]=='-')num=(-1)*num;
        return num;
+    }
+
+
+APPROACH 2:
+
+int atoi(string s) {
+        //Your code here
+        int ans = 0;
+        int sign = 1;
+        
+        for(int i = 0; i < s.size(); i++) {
+            int ch = s[i];
+            if(i == 0 && ch == '-') sign = -1;
+            else if(ch >= '0' && ch <= '9') {
+                int digit = ch - '0';
+                ans = ans*10 + digit;
+            }
+            else {
+                return -1;
+            }
+        }
+        
+        return sign*ans;
+    
     }
