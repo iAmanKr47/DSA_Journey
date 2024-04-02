fb0e2863d333a641c62906a665c6e1b9de1e675b
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-04-03 00:34:15 +0530

    Create 1518. Water Bottles.cpp

---
+++ Mathematical Ques/1518. Water Bottles.cpp
@@ -0,0 +1,17 @@
+int numWaterBottles(int numBottles, int numExchange) {
+        int maxDrinkBottle=0;
+        int emptyBottles=0;
+        int fullBottles=numBottles;
+        while(fullBottles!=0||emptyBottles>=numExchange){
+            if(fullBottles>0){
+                emptyBottles+=fullBottles;
+                maxDrinkBottle+=fullBottles;
+                fullBottles=0;
+            }
+            else{
+               fullBottles+=  emptyBottles/numExchange;
+                emptyBottles= emptyBottles%numExchange;
+            }
+        }
+        return maxDrinkBottle;
+    }
