5cf47001204e8c47c9547a72a59a193affd7fa94
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-05-18 03:17:37 +0530

    Create 1325. Delete Leaves With a Given Value.cpp

---
+++ TREES/1325. Delete Leaves With a Given Value.cpp
@@ -0,0 +1,10 @@
+TreeNode* removeLeafNodes(TreeNode* root, int target) {
+    if(root==NULL)return NULL;
+    root->left=removeLeafNodes(root->left,target);
+    root->right=removeLeafNodes(root->right,target);
+    if( root->val==target && root->right==NULL && root->left==NULL) 
+    {
+        return NULL;
+    }
+    return root;
+    }
