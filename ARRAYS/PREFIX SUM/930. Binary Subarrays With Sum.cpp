0608349596816ebc5b6494b1ba783ed0511513b8
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-06-22 05:58:47 +0530

    Create 930. Binary Subarrays With Sum.cpp

---
+++ ARRAYS/PREFIX SUM/930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,15 @@
+//same concept is used in:  1248. Count Number of Nice Subarrays
+ int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int>mp;
+        int sum=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==goal)cnt++;
+            if(mp.find(sum-goal)!=mp.end()){
+                cnt+=mp[sum-goal];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
