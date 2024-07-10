ea1a7d9883ab71691c2b680c7a73eebbd64af33a
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-07-10 09:02:09 +0530

    Create 1701. Average Waiting Time.cpp

---
+++ ARRAYS/1701. Average Waiting Time.cpp
@@ -0,0 +1,23 @@
+
+double averageWaitingTime(vector<vector<int>>& customers) {
+        int n=customers.size();
+        int currTime=customers[0][0]+customers[0][1];
+        long long waiting=customers[0][1];
+        for(int i=1;i<n;i++){
+            int arrivalTime=customers[i][0];
+            int orderPrepareTime=customers[i][1];
+            waiting+=orderPrepareTime;
+            
+            if(currTime<arrivalTime){
+                currTime=arrivalTime;
+                currTime+=orderPrepareTime;
+            }
+            else{
+                waiting+=currTime-arrivalTime;
+                currTime+=orderPrepareTime;
+            }
+            
+
+        }
+        return (double)waiting/n;
+    }
