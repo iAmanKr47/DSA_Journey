48db674ff0bef791aeefd19dd59b08ad964b19b3
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-08-12 01:01:57 +0530

    Update Maximizing Productivity.cpp

--- CONTEST/GFG CC-167/Maximizing Productivity.cpp
+++ CONTEST/GFG CC-167/Maximizing Productivity.cpp
@@ -25,3 +25,29 @@ class Solution {
         return maxi;
     }
 };
+
+
+//code 2 :Same with return type of int
+
+class Solution {
+  public:
+  int maxi=0;
+  int solve(int index,int sum1,int &totalSum,int &n,vector<int> &productivity,vector<int>&dp){
+      if(index>=n){
+          return sum1*(totalSum-sum1);
+      }
+      if(dp[sum1]!=-1)return dp[sum1];
+      
+      int take=solve(index+1,sum1+productivity[index],totalSum,n,productivity,dp);
+      int notTake=solve(index+1,sum1,totalSum,n,productivity,dp);
+      return dp[sum1]=max(take,notTake);
+  }
+    int getMaxProductivity(int n, vector<int> &productivity) {
+        int sum1=0;
+        int totalSum=0;
+        for(auto i:productivity)totalSum+=i;
+        vector<int>dp(totalSum+1,-1);
+        return solve(0,sum1,totalSum,n,productivity,dp);
+
+    }
+};
