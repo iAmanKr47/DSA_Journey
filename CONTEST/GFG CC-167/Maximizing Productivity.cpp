b40399cd5539e3c7827472dd804deebc31514934
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-08-12 00:58:19 +0530

    Create Maximizing Productivity.cpp

---
+++ CONTEST/GFG CC-167/Maximizing Productivity.cpp
@@ -0,0 +1,27 @@
+// https://practice.geeksforgeeks.org/contest/gfg-weekly-167-rated-contest/problems
+// PICK - NOT PICK APPROACH
+
+class Solution {
+  public:
+  int maxi=0;
+  void solve(int index,int sum1,int &totalSum,int &n,vector<int> &productivity,vector<int>&dp){
+      if(index>=n){
+          maxi=max(maxi,sum1*(totalSum-sum1));
+          dp[sum1]=maxi;
+          return;
+      }
+      if(dp[sum1]!=-1)return ;
+      
+      solve(index+1,sum1+productivity[index],totalSum,n,productivity,dp);
+      solve(index+1,sum1,totalSum,n,productivity,dp);
+  }
+    int getMaxProductivity(int n, vector<int> &productivity) {
+        int sum1=0;
+        int sum2=0;
+        int totalSum=0;
+        for(auto i:productivity)totalSum+=i;
+        vector<int>dp(totalSum+1,-1);
+        solve(0,sum1,totalSum,n,productivity,dp);
+        return maxi;
+    }
+};
