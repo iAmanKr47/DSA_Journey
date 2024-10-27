f4500fba507fa0c2351412d298b8c2efeb3dd06e
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-10-27 11:50:14 +0530

    Create 221. Maximal Square***.cpp

---
+++ DYNAMIC PROGRAMMING/221. Maximal Square***.cpp
@@ -0,0 +1,24 @@
+int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+         int m = matrix[0].size();
+         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         int maxi = 0;
+         for(int i = n-1;i>=0;i--)
+         {
+            for(int j = m-1;j>=0;j--)
+            {
+                 int right = dp[i][j+1];
+                 int diagonal = dp[i+1][j+1];
+                 int down = dp[i+1][j];
+                 
+                 if(matrix[i][j]=='1')
+                 {
+                    int ans = 1+min(right,min(diagonal,down));
+                    maxi = max(ans,maxi);
+                    dp[i][j]=ans;
+                 }
+            }
+         }
+
+         return maxi*maxi;
+    }
