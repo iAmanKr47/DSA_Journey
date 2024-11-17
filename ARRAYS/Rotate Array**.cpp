8837f66664e6d286c414675aee4bf95f4911ed57
AMAN KUMAR <104636491+iAmanKr47@users.noreply.github.com>
2024-11-18 00:30:16 +0530

    Create Rotate Array**.cpp

---
+++ ARRAYS/Rotate Array**.cpp
@@ -0,0 +1,13 @@
+
+// Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code 
+        int n = arr.size();
+        d %= n; //when d>=n
+        // First reversing d elements from starting index.
+        reverse(arr.begin(), arr.begin() + d);
+        // Then reversing the last n-d elements.
+        reverse(arr.begin() + d, arr.begin() + n);
+        // Finally, reversing the whole array.
+        reverse(arr.begin(), arr.begin() + n);
+    }
